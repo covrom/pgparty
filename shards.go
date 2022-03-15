081c07153ae56f557a33e33fd528cc152814e092
Roman <covrom@gmail.com>
2022-03-15 22:58:12 +0300

    shards context shortcuts

--- shards.go
+++ shards.go
@@ -20,6 +20,51 @@ func NewShards(ctx context.Context) (*Shards, context.Context) {
 	return shs, WithShards(ctx, shs)
 }
 
+func SetShard(ctx context.Context, id string, db *sqlx.DB, schema string) (Shard, error) {
+	shs, err := ShardsFromContext(ctx)
+	if err != nil {
+		return Shard{}, err
+	}
+	return shs.SetShard(id, db, schema), nil
+}
+
+func ShardByID(ctx context.Context, id string) (Shard, error) {
+	shs, err := ShardsFromContext(ctx)
+	if err != nil {
+		return Shard{}, err
+	}
+	ret, ok := shs.ShardByID(id)
+	if ok {
+		return ret, nil
+	}
+	return Shard{}, fmt.Errorf("shard %q not found", id)
+}
+
+func SelectShard(ctx context.Context, id string) (context.Context, error) {
+	s, err := ShardByID(ctx, id)
+	if err != nil {
+		return ctx, err
+	}
+	return WithShard(ctx, s), nil
+}
+
+func DeleteShard(ctx context.Context, id string) error {
+	shs, err := ShardsFromContext(ctx)
+	if err != nil {
+		return err
+	}
+	shs.DeleteShard(id)
+	return nil
+}
+
+func WalkShards(ctx context.Context, f func(Shard) error) error {
+	shs, err := ShardsFromContext(ctx)
+	if err != nil {
+		return err
+	}
+	return shs.Walk(f)
+}
+
 func (s *Shards) SetShard(id string, db *sqlx.DB, schema string) Shard {
 	s.Lock()
 	defer s.Unlock()
