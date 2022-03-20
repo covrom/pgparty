34467085bc78b35af61fe5567ad4d28f08ccd9d5
Roman <covrom@gmail.com>
2022-03-20 18:06:22 +0300

    basic test comments

--- pgparty_test/basic_test.go
+++ pgparty_test/basic_test.go
@@ -21,20 +21,35 @@ func TestBasicUsage(t *testing.T) {
 		log.Fatal("run TestMain before")
 	}
 
+	// create shards repository with only one database connect
 	shs, ctx := pgparty.NewShards(pgparty.WithLoggingQuery(context.Background()))
 
 	shard := shs.SetShard("shard1", db, "shard1")
 
+	// register models in shard
 	if err := pgparty.Register(shard, pgparty.MD[BasicModel]{}); err != nil {
 		t.Errorf("pgparty.Register error: %s", err)
 		return
 	}
 
+	// migrate models in database to current structure
 	if err := shard.Migrate(ctx, nil); err != nil {
 		t.Errorf("shard.Migrate error: %s", err)
 		return
 	}
+	// this produces sql queries:
+	// CREATE TABLE shard1.basic_models (data jsonb,id UUID NOT NULL,PRIMARY KEY (id))
+	// INSERT INTO shard1._config (table_name,storej)
+	// 	VALUES($1,$2) ON CONFLICT(table_name) DO
+	// 	UPDATE SET storej=excluded.storej
 
+	// $1 =  basic_models ,
+	// $2 =  {"table":"basic_models","cols":[{"ColName":"data","DataType":"jsonb","DefaultValue":"","NotNull":false,"PrimaryKey":false},
+	// {"ColName":"id","DataType":"UUID","DefaultValue":"","NotNull":true,"PrimaryKey":true}]}
+
+	// future migrations use this '_config' table for building differencies as ALTER DDL queries
+
+	// create a model element
 	el := BasicModel{
 		ID: pgparty.NewV4(),
 		Data: *pgparty.NewNullJsonB(map[string]any{
@@ -44,20 +59,26 @@ func TestBasicUsage(t *testing.T) {
 		}),
 	}
 
+	// replace it in database by id
 	if err := pgparty.WithTxInShard(ctx, shard.ID, func(ctx context.Context) error {
 		return pgparty.Replace[BasicModel](ctx, el)
 	}); err != nil {
 		t.Errorf("pgparty.Replace error: %s", err)
 		return
 	}
+	// this produces sql queries:
+	// INSERT INTO shard1.basic_models (id,data) VALUES($1,$2) ON CONFLICT(id) DO UPDATE SET data=excluded.data
 
+	// select stored data from model table
 	var els []BasicModel
 	if err := shard.WithTx(ctx, func(ctx context.Context) error {
-		return pgparty.Select[BasicModel](ctx, `SELECT * FROM &BasicModel`, &els)
+		return pgparty.Select[BasicModel](ctx, `SELECT * FROM &BasicModel`, &els) // &BasicModel - model named by golang struct type name
 	}); err != nil {
 		t.Errorf("pgparty.Select error: %s", err)
 		return
 	}
+	// this produces sql queries:
+	// SELECT * FROM shard1.basic_models
 
 	if els[0].ID != el.ID {
 		t.Errorf("pgparty.Select error: els[0].ID != el.ID: %s != %s", els[0].ID, el.ID)
