f73dd0fcc032089456d31625dbe61d51d5f5b36b
Roman <covrom@gmail.com>
2022-11-01 11:06:57 +0300

    Viewable interface

--- ifaces.go
+++ ifaces.go
@@ -1,6 +1,18 @@
 package pgparty
 
-// Storable - интерфейс, которая должна реализовывать структура, которая должна храниться в базе.
+// Storable is an interface that the model structure must implement
 type Storable interface {
 	StoreName() string
+}
+
+// Viewable is an interface that the view-model structure must implement
+type Viewable interface {
+	Storable
+	ViewQuery() string
+}
+
+// MaterializedViewable is an interface that the materialized view-model structure must implement
+type MaterializedViewable interface {
+	Viewable
+	MaterializedView() // not called, define with empty body
 }
