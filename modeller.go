0d8b649d35a7f5cf56a50ec318481413c0b39453
Roman Tsovanyan <covrom@gmail.com>
2024-08-04 13:10:52 +0300

    Modeller

--- modeller.go
+++ modeller.go
@@ -1,14 +1,6 @@
 package pgparty
 
-import "reflect"
-
-type StructField struct {
-	Name string
-	Type      reflect.Type      // field type
-	Tag       reflect.StructTag // field tag string
-}
-
 type Modeller interface {
 	NumField() int
-	Field(i int) StructField
+	Field(i int) *FieldDescription
 }
