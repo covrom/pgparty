7d2b052c769627239a9832d6290f9152377c8302
Roman Tsovanyan <covrom@gmail.com>
2024-08-04 13:15:04 +0300

    Modeller

--- modeller.go
+++ modeller.go
@@ -1,6 +1,12 @@
 package pgparty
 
+import "reflect"
+
 type Modeller interface {
+	ReflectType() reflect.Type
+	TypeName() string
+	DatabaseName() string
 	NumField() int
 	Field(i int) *FieldDescription
+	WalkFields(f func(i int, fd *FieldDescription) error) error
 }
